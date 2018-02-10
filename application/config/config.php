cc8d02aa34a7a99fd24d05d391573a94dbb6f21e
tester.kottayam <tester.kottayam@valintechnologies.in>
2018-02-10 15:36:04 +0530

    correction

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/SORT_CODEIG/';
+date_default_timezone_set('Asia/Kolkata');
+$config['base_url'] = 'http://192.168.1.3/sort/';
 
 /*
 |--------------------------------------------------------------------------
